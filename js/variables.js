0b92bd86e47145f56863580be127846c0094e7bf
ganesh <ganeshsadhashivam@outlook.com>
2023-05-10 14:43:18 +0530

    jsRefresh

---
+++ js/variables.js
@@ -0,0 +1,11 @@
+//named export
+// export const num1 = 5;
+// export const num2 = 10;
+
+const num1 = 5;
+export const num2 = 10;
+
+//export { num1, num2 };
+
+//default exports
+export default num1;
---
+++ script.js
@@ -0,0 +1,11 @@
+// import { num1, num2 } from "./js/variables.js";
+
+// import * as vare from "./js/variables.js";
+// import { add } from "./js/functions.js";
+// console.log(vare.num1, vare.num2);
+
+// console.log(add(vare.num1, vare.num2));
+
+import num1, { num2 } from "./js/variables.js";
+
+console.log(num1, num2);
